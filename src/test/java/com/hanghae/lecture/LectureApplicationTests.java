a18b401c46c9f21d2f97a6f48cb457fd4ab5a499
박영준\qdw07 <qdw0719@naver.com>
2024-06-23 14:13:53 +0900

    Spring initialize, setting

---
+++ src/test/java/com/hanghae/lecture/LectureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanghae.lecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
